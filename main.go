ede98d110269192b43baf830be3fd7be5237de82
Loai Al-Sharee <loaialsharee1@gmail.com>
2024-01-26 23:06:31 +0800

    Add simple GET/POST requests & API http test data

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"net/http"
+
+	"example.com/models"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := gin.Default()
+
+	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
+
+	server.Run(":8080") //localhost:8080
+
+}
+
+func getEvents(ctx *gin.Context) {
+	events := models.GetAllEvents()
+	ctx.JSON(http.StatusOK, events)
+}
+
+func createEvent(ctx *gin.Context) {
+	var event models.Event
+	err := ctx.ShouldBindJSON(&event)
+
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data."})
+		return
+	}
+
+	event.Id = 1
+	event.UserId = 1
+
+	event.Save()
+
+	ctx.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
+}
