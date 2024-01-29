764e943b7320bcb6d23c5922a679d381e18ff4f9
Loai Al-Sharee <loaialsharee1@gmail.com>
2024-01-29 20:58:29 +0800

    Get single event

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"example.com/db"
 	"example.com/models"
@@ -13,10 +14,28 @@ func main() {
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
+	server.GET("/events/:id", getEvent)
 	server.POST("/events", createEvent)
 
 	server.Run(":8080") //localhost:8080
 
+}
+
+func getEvent(ctx *gin.Context) {
+	eventId, err := strconv.ParseInt(ctx.Param("id"), 10, 64)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": "Could not fetch event id"})
+		return
+	}
+
+	event, err := models.GetEventById(eventId)
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch event id"})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, event)
 }
 
 func getEvents(ctx *gin.Context) {
